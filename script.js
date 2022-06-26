e0118174d51ea42acd135cb82f07d582ee4def54
Phinjock Sherpa <phinjock@gmail.com>
2022-06-25 17:27:46 -0700

    fixed codes

--- script.js
+++ script.js
@@ -2,55 +2,50 @@
 
 
 // What is undefined? do more research on loop and math.random
-var alphabet="abcdefghijklmnopqrstuvwxyz";
-  var upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric=["1","2","3","4","5","6","7","8","9","0"];
+var alphabet="abcdefghijklmnopqrstuvwxyz"
+  var upper=alphabet.toUpperCase();
+  var numeric="123456789"
   var specialCharacter="^&!@$-_*";
   let passwordText='';
 
 var generatePassword=function (){
   
+
  var v=prompt("Would you like a.uppercase or b.lowercase alphabet or c.both in your password? Please select a, b or c")
   if(v==="a"){
-  v="upper";
-  console.log(upper)
+  v;  
+  v=upper; 
   }else if(v==="b"){
-  v="abcdefghijklmnopqrstuvwxyz";
-  console.log(alphabet)
-  }else if(v==="c"){
   v=alphabet;
-  console.log(alphabet.concat(upper))
-  }else{
+    }else if(v==="c"){
+  v=alphabet.concat(upper);
+    }else{
    window.alert("please select from a, b or c")
-  }
-  if (--v> 0)
-  return v;
+   return passwordText;
+  } 
+  
   
   var w=prompt("Would you like numbers in your password y or n?")
   if(w==="y"){
-  w=["1","2","3","4","5","6","7","8","9","0"];
-  console.log(numeric)
+  w = numeric;
   }else if(w==="n"){
   w=""
   console.log(v)
   }else{
   window.alert("please select Y or N?")
+  return passwordText;
   }
-  if (--w > 0)
-  return w; 
-
+  
   var x=prompt("Would you like symbols in your password y or n?")
   if(x==="y"){
-  x="^&*";
-  console.log(specialCharacter)
+  x = specialCharacter;
   }else if(x==="n"){
   x=""
   }else{
   window.alert("please select Yes or No?")
+  return passwordText;
   }
-  if (--x > 0)
-  return x;
-   
+ 
 
   var y=prompt("What would you like your password length to be(minimum-8 to maximum-128)")
   if(y>= 8 && y <= 128){
@@ -60,14 +55,15 @@ var generatePassword=function (){
   }
   else{
   alert("make sure the length is between 8-128 characters long")
+  return generatePassword;
   }
- 
+  
   console.log(y)
 
-  var characters = v + w + x;
-  console.log(characters)
+ 
+  var characters = v+x+w
   for(var i, i=0;i<y;i++){
-  let generatePassword=characters[Math.floor(Math.random()* y)];
+  let generatePassword=characters.charAt(Math.floor(Math.random()* y));
   passwordText += generatePassword
  }
  return passwordText;
