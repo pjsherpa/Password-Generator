00520321dea8637ed8d706c83c929b7edb43adfd
Phinjock Sherpa <phinjock@gmail.com>
2022-06-26 12:25:29 -0700

    retouch on comments

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var numeric="123456789"
 var specialCharacter="~@#$!%^&*_-+=;<,>.?/";
 
 function generatePassword(){
-  // passwordText is what we will need to find. we kept this in the local scope and not the global scope as it will just add into the password previously created.-PJ
+  // passwordText is what I will need to find. I kept this in the local scope and not the global scope as it will just add into the password previously created.-PJ
   var passwordText='';
   // first set of question for password criteria is to give a value to v, which will include the type of alphabet lower/upper case or both.-PJ
   var v=prompt("Would you like a.uppercase or b.lowercase alphabet or c.both in your password? Please select a, b or c")
@@ -32,23 +32,23 @@ function generatePassword(){
   w=""
   console.log(v)
   }else{
-  window.alert("please select Y or N")
+  window.alert("please select y or n")
   return;
   }
   
   // third set of question for password criteria is to give a value to x, which will include the specialCharacters or exclude it depending on what is selected.-PJ
-  var x=prompt("Would you like symbols in your password y or n?")
+  var x=prompt("Would you like symbols in your password y for Yes or n for No?")
   if(x==="y"){
   x = specialCharacter;
   }else if(x==="n"){
   x=""
   }else{
-  window.alert("please select Yes or No")
+  window.alert("please select y for Yes or n for No")
   return;
   }
  
 // last set of question for password criteria is to give a value to y, which will be our length-PJ
-  var y=prompt("What would you like your password length to be?(minimum-8 to maximum-128)")
+  var y=prompt("What would you like your password length to be?(Please select from minimum-8 to maximum-128)")
   if(y>= 8 && y <= 128){
   y;
   let length=Number(y);
@@ -56,12 +56,12 @@ function generatePassword(){
   console.log(typeof length)
   }
   else{
-  alert("make sure the length is between 8-128 characters long")
+  alert("Please make sure the length is between 8-128 characters long")
   return;
   }
   
   /*for the final step I have concatinated v,w,x into one variable named characters. 
-  I have assigned the length which we got from var y on the loop and then assigned both the character and lenght to math.random function to generate the password - PJ*/
+  I have assigned the length which I got from var y on the loop and then assigned both the character and lenght to math.random function to generate the password - PJ*/
 
   var characters = v+x+w;
   for(var i, i=0;i<y;i++){
